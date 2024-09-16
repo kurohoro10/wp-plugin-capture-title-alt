23a4a1c02ca6fb7133e0f40f0641183450a95fd2
kurohoro10 <kurohoro10@gmail.com>
2024-09-16 11:46:10 +0800

    Initial commit to a wp plugin

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a plugin that captures the very first headings and paragraph of the image and make it the title and the alt text of that said image.
---
+++ wp-capture-titlealt.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     WP Capture Title and Alt Plugin
+ * Plugin URI:      
+ * Description:     Plugin that captures titles and alt text for images
+ * Author:          
+ * Author URI:      
+ * Text Domain:     wp-capture-titlealt
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Capture_Titlealt
+ */
+
+// Your code starts here.
